35cd2d9ed707ba0557f1ce80d48cc3d208c4dc06
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2020-11-23 18:10:13 +0700

    add remote invoker?

--- video_test.py
+++ video_test.py
@@ -2,20 +2,23 @@ import lithops
 import ffmpeg
 from storage_util import upload_file
 
-def process_video(output_name):
-    input = ffmpeg.input('http://techslides.com/demos/sample-videos/small.mp4')
+input = ['http://techslides.com/demos/sample-videos/small.mp4']
+
+def process_video(input_url):
+    input = ffmpeg.input(input_url)
     audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
     video = input.video.hflip()
     out = ffmpeg.output(audio, video, 'output.mp4')
     ffmpeg.run(out)
     f = open('output.mp4','rb')
-    upload_file("test-bucket-fn",output_name,f)
+    upload_file("test-bucket-fn",'output-new.mp4',f)
     print('finish uploading')
     f.close()
 
 if __name__ == '__main__':
     fexec = lithops.FunctionExecutor()
-    fexec.call_async(process_video,'output.mp4')
+    # fexec.call_async(process_video,'output.mp4')
+    fexec.map(process_video,input)
     print(fexec.get_result())
 
 
